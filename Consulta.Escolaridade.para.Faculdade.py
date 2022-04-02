19a58614511743f581c5d53cdea351a06893d7d6
Rafael Domingues Leal <87152989+Rafael-D-Leal@users.noreply.github.com>
2022-04-02 02:34:22 -0300

    Initial commit

---
+++ Consulta.Escolaridade.para.Faculdade.py
@@ -0,0 +1,26 @@
+"//*Adentrar a faculdade*//"
+
+nome = input("Nome: ")
+escolaridade = int(input("Terminou o ensino médio (1 - SIM/ 2 - NÃO): "))
+nivel = int(escolaridade)
+
+if nivel == 1:
+    print(Nome + " bem bindo à universidade!")
+else:
+    print(Nome + " aguarde, logo será sua vez")
+print("Amar a Deus sobre todas as coisas e ao próximo como a ti mesmo")
+print("FIM")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
